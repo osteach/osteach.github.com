1324331d1bbf54e3a1f1db353b5cdd330caa2a40
suzie <monkeysuzie@gmail.com>
2012-12-30 00:15:53 +0800

    add fork图片 和 自由软件和开源软件

--- assets/js/list.js
+++ assets/js/list.js
@@ -1,6 +1,6 @@
 var list = {
 
-	"Manual": {
+	"绪论": {
 		"介绍": [
 			[ "李小龙", "manual/introduction/BruceLee" ],
 			[ "调查", "manual/introduction/Research" ],
@@ -12,8 +12,13 @@ var list = {
 		]
 	},
 	"开源文化": {
-		"Introduction": [
-			[ "软件天生就是自由的", "class/freesoftware/Freesoftware" ]
+		"课程": [
+			[ "软件天生就是自由的", "class/freesoftware/Freesoftware" ],
+			[ "RMS 与 GNU", "class/freesoftware/RMS&GNU" ],
+			[ "开源软件_理想和现实的折衷", "class/freesoftware/Opensource" ],
+			[ "开源软件与自由软件", "class/freesoftware/OS&Free" ],
+			[ "软件分类", "class/freesoftware/Classification" ],
+			[ "开源软件许可证", "class/freesoftware/License" ]
 
 		]
 	},
@@ -28,166 +33,10 @@ var list = {
 		],
 
 		"人物": [
-			[ "阿兰·图灵", "opensource/characters/Turing" ]
-		],
-
-		"Lights": [
-			[ "Light", "api/lights/Light" ],
-			[ "AmbientLight", "api/lights/AmbientLight" ],
-			[ "DirectionalLight", "api/lights/DirectionalLight" ],
-			[ "PointLight", "api/lights/PointLight" ],
-			[ "SpotLight", "api/lights/SpotLight" ]
-		],
-
-
-		"Loaders": [
-			[ "Loader", "api/loaders/Loader" ],
-			[ "BinaryLoader", "api/loaders/BinaryLoader" ],
-			[ "GeometryLoader", "api/loaders/GeometryLoader" ],
-			[ "ImageLoader", "api/loaders/ImageLoader" ],
-			[ "JSONLoader", "api/loaders/JSONLoader" ],
-			[ "LoadingMonitor", "api/loaders/LoadingMonitor" ],
-			[ "SceneLoader", "api/loaders/SceneLoader" ],
-			[ "TextureLoader", "api/loaders/TextureLoader" ],
-		],
-
-		"Materials": [
-			[ "Material", "api/materials/Material" ],
-			[ "LineBasicMaterial", "api/materials/LineBasicMaterial" ],
-			[ "MeshBasicMaterial", "api/materials/MeshBasicMaterial" ],
-			[ "MeshDepthMaterial", "api/materials/MeshDepthMaterial" ],
-			[ "MeshFaceMaterial", "api/materials/MeshFaceMaterial" ],
-			[ "MeshLambertMaterial", "api/materials/MeshLambertMaterial" ],
-			[ "MeshNormalMaterial", "api/materials/MeshNormalMaterial" ],
-			[ "MeshPhongMaterial", "api/materials/MeshPhongMaterial" ],
-			[ "ParticleBasicMaterial", "api/materials/ParticleBasicMaterial" ],
-			[ "ParticleCanvasMaterial", "api/materials/ParticleCanvasMaterial" ],
-			[ "ParticleDOMMaterial", "api/materials/ParticleDOMMaterial" ],
-			[ "ShaderMaterial", "api/materials/ShaderMaterial" ]
-
-		],
-
-		"Objects": [
-			[ "Bone", "api/objects/Bone" ],
-			[ "Line", "api/objects/Line" ],
-			[ "LOD", "api/objects/LOD" ],
-			[ "Mesh", "api/objects/Mesh" ],
-			[ "MorphAnimMesh", "api/objects/MorphAnimMesh" ],
-			[ "Particle", "api/objects/Particle" ],
-			[ "ParticleSystem", "api/objects/ParticleSystem" ],
-			[ "Ribbon", "api/objects/Ribbon" ],
-			[ "SkinnedMesh", "api/objects/SkinnedMesh" ],
-			[ "Sprite", "api/objects/Sprite" ]
-
-		],
-
-		"Renderers": [
-			[ "CanvasRenderer", "api/renderers/CanvasRenderer" ],
-			[ "DOMRenderer", "api/renderers/DOMRenderer" ],
-			[ "SVGRenderer", "api/renderers/SVGRenderer" ],
-			[ "WebGLRenderer", "api/renderers/WebGLRenderer" ],
-			[ "WebGLRenderTarget", "api/renderers/WebGLRenderTarget" ],
-			[ "WebGLRenderTargetCube", "api/renderers/WebGLRenderTargetCube" ],
-			[ "WebGLShaders", "api/renderers/WebGLShaders" ]
-		],
-
-		"Renderers / Renderables": [
-			[ "RenderableFace3", "api/renderers/renderables/RenderableFace3" ],
-			[ "RenderableFace4", "api/renderers/renderables/RenderableFace4" ],
-			[ "RenderableLine", "api/renderers/renderables/RenderableLine" ],
-			[ "RenderableObject", "api/renderers/renderables/RenderableObject" ],
-			[ "RenderableParticle", "api/renderers/renderables/RenderableParticle" ],
-			[ "RenderableVertex", "api/renderers/renderables/RenderableVertex" ]
-		],
-
-		"Scenes": [
-			[ "Fog", "api/scenes/Fog" ],
-			[ "FogExp2", "api/scenes/FogExp2" ],
-			[ "Scene", "api/scenes/Scene" ]
-		],
-
-		"Textures": [
-			[ "DataTexture", "api/textures/DataTexture" ],
-			[ "Texture", "api/textures/Texture" ]
-		],
-
-		"Extras": [
-			[ "ColorUtils", "api/extras/ColorUtils" ],
-			[ "GeometryUtils", "api/extras/GeometryUtils" ],
-			[ "ImageUtils", "api/extras/ImageUtils" ],
-			[ "SceneUtils", "api/extras/SceneUtils" ],
-			[ "ShaderUtils", "api/extras/ShaderUtils" ]
-		],
-
-		"Extras / Animation": [
-			[ "Animation", "api/extras/animation/Animation" ],
-			[ "AnimationHandler", "api/extras/animation/AnimationHandler" ],
-			[ "AnimationMorphTarget", "api/extras/animation/AnimationMorphTarget" ],
-			[ "KeyFrameAnimation", "api/extras/animation/KeyFrameAnimation" ]
-		],
-
-		"Extras / Cameras": [
-			[ "CombinedCamera", "api/extras/cameras/CombinedCamera" ],
-			[ "CubeCamera", "api/extras/cameras/CubeCamera" ]
-		],
-
-		"Extras / Core": [
-			[ "BufferGeometry", "api/extras/core/BufferGeometry" ],
-			[ "Curve", "api/extras/core/Curve" ],
-			[ "CurvePath", "api/extras/core/CurvePath" ],
-			[ "EventTarget", "api/extras/core/EventTarget" ],
-			[ "Gyroscope", "api/extras/core/Gyroscope" ],
-			[ "Path", "api/extras/core/Path" ],
-			[ "Shape", "api/extras/core/Shape" ],
-			[ "TextPath", "api/extras/core/TextPath" ]
-		],
-
-		"Extras / Geometries": [
-			[ "CubeGeometry", "api/extras/geometries/CubeGeometry" ],
-			[ "CylinderGeometry", "api/extras/geometries/CylinderGeometry" ],
-			[ "ExtrudeGeometry", "api/extras/geometries/ExtrudeGeometry" ],
-			[ "IcosahedronGeometry", "api/extras/geometries/IcosahedronGeometry" ],
-			[ "LatheGeometry", "api/extras/geometries/LatheGeometry" ],
-			[ "OctahedronGeometry", "api/extras/geometries/OctahedronGeometry" ],
-			[ "PlaneGeometry", "api/extras/geometries/PlaneGeometry" ],
-			[ "PolyhedronGeometry", "api/extras/geometries/PolyhedronGeometry" ],
-			[ "SphereGeometry", "api/extras/geometries/SphereGeometry" ],
-			[ "TetrahedronGeometry", "api/extras/geometries/TetrahedronGeometry" ],
-			[ "TextGeometry", "api/extras/geometries/TextGeometry" ],
-			[ "TorusGeometry", "api/extras/geometries/TorusGeometry" ],
-			[ "TorusKnotGeometry", "api/extras/geometries/TorusKnotGeometry" ]
-		],
-
-		"Extras / Helpers": [
-			[ "AxisHelper", "api/extras/helpers/AxisHelper" ],
-			[ "CameraHelper", "api/extras/helpers/CameraHelper" ]
-		],
-
-		"Extras / Modifiers": [
-			[ "SubdivisionModifier", "api/extras/modifiers/SubdivisionModifier" ]
-		],
-
-		"Extras / Objects": [
-			[ "LensFlare", "api/extras/objects/LensFlare" ]
-		],
-
-		"Extras / Renderers / Effects": [
-			[ "AnaglyphEffect", "api/extras/renderers/effects/AnaglyphEffect" ],
-			[ "CrosseyedEffect", "api/extras/renderers/effects/CrosseyedEffect" ],
-			[ "ParallaxBarrierEffect", "api/extras/renderers/effects/ParallaxBarrierEffect" ],
-			[ "StereoEffect", "api/extras/renderers/effects/StereoEffect" ]
-		],
-
-		"Extras / Renderers / Plugins": [
-			[ "LensFlarePlugin", "api/extras/renderers/plugins/LensFlarePlugin" ],
-			[ "ShadowMapPlugin", "api/extras/renderers/plugins/ShadowMapPlugin" ],
-			[ "SpritePlugin", "api/extras/renderers/plugins/SpritePlugin" ]
-		],
-
-		"Extras / Shaders": [
-			[ "ShaderFlares", "api/extras/shaders/ShaderFlares" ],
-			[ "ShaderSprite", "api/extras/shaders/ShaderSprite" ]
+			[ "阿兰·图灵", "opensource/characters/Turing" ],
+			[ "RMS", "opensource/characters/RMS" ]
 		]
+
 	}
 
 };
